6b11e527afc54ed7a9c12ba1d7c1968a0262e23d
PeterHung26 <116525672+PeterHung26@users.noreply.github.com>
2025-07-10 11:36:50 -0700

    Define some types

---
+++ ahb_mtrxn.sv
@@ -0,0 +1,3 @@
+class ahb_mtrxn extends uvm_sequence_item;
+    // This file defines the AHB master transaction class
+endclass: ahb_mtrxn
---
+++ ahb_types_pkg.sv
@@ -0,0 +1,31 @@
+package ahb_types_pkg;
+
+    // This package defines the AHB types used in the AHB master agent
+    typedef enum logic [1:0] {
+        IDLE,
+        BUSY,
+        NONSEQ,
+        SEQ
+    } transfer_t;
+
+    typedef enum logic [2:0] {
+        SINGLE,
+        INCR,
+        WRAP4,
+        INCR4,
+        WRAP8,
+        INCR8,
+        WRAP16,
+        INCR16
+    } burst_t;
+
+    typedef enum logic{
+        OKAY,
+        ERROR
+    } resp_t;
+
+    typedef enum logic{
+        READ,
+        WRITE
+    } rw_t;
+endpackage
